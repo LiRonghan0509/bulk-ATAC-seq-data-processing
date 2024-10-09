24d1ecfdf2223adf7a547d41d8d81f5d22222be6
Ronghan Li <146203214+LiRonghan0509@users.noreply.github.com>
2024-10-09 14:59:07 -0500

    Update and rename 0.data_splitbam.sh to 0.data_tn5shift.sh

--- visualization/0.data_tn5shift.sh
+++ visualization/0.data_tn5shift.sh
@@ -1,5 +1,20 @@
-# Split .bam files into .chr.bam files
 #!/bin/bash
+
+# TN5 shifting of tagaligns for ATACseq
+
+mkdir tagalign_bed_files
+mkdir tn5_bed_files
+
+for bam in filtered_bam_files/*.bam
+do
+    prefix=$(basename $bam .bam)
+    bedtools bamtobed -i $bam > tagalign_bed_files/${prefix}.tagAlign.bed
+    awk 'BEGIN {OFS = "\t"} {if ($6 == "+") $2 = $2 + 4; else if ($6 == "-") $3 = $3 - 5; print}' tagalign_bed_files/${prefix}.tagAlign.bed > tn5_bed_files/${prefix}.tn5.tagAlign.bed
+    gzip tn5_bed_files/${prefix}.tn5.tagAlign.bed
+done
+
+
+# Split .bam files into .chr.bam files
 samples=("$@")
 output_dir="split_bam_files"
 mkdir -p $output_dir
